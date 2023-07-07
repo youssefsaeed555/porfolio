afe9c0cd4b67caf518d817b59ba9376bf06af23b
youssef saeed <yossefsaid555@gmail.com>
2023-07-08 00:48:26 +0300

    edit banner and home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -57,22 +57,21 @@ function Home() {
           style={{
             transform: isInView ? "none" : "translateY(-200px)",
             opacity: isInView ? 1 : 0,
-            transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
           }}
         >
           {skills}
         </TagCloud>
       </div>
-      <div className="mt-3 flex justify-content-center gap-24 flex-wrap max-[500px]:flex-col">
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all .5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+      <div
+        style={{
+          transform: isInView ? "none" : "translateY(-200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 3.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
+        }}
+        className="mt-3 flex justify-content-center gap-24 flex-wrap max-[500px]:flex-col"
+      >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
@@ -82,15 +81,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">React</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all .6s cubic-bezier(0.17, 0.55, 0.55, 1) 0.6s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
@@ -100,15 +91,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">Typescript</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all .7s cubic-bezier(0.17, 0.55, 0.55, 1) 0.7s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://files.raycast.com/4dnlt8m2mcb98bzc4zb8pggc4csi"
@@ -118,15 +101,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">Nextjs</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all .8s cubic-bezier(0.17, 0.55, 0.55, 1) 0.8s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://camo.githubusercontent.com/9eecc42439347332f256a326363924551042f5b96235f972982512199476611a/68747470733a2f2f616e67756c61722e696f2f6173736574732f696d616765732f6c6f676f732f616e67756c61722f616e67756c61722e737667"
@@ -136,15 +111,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">Angular</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all .9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.9s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
@@ -154,15 +121,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">JS</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1)  1s",
-          }}
-          className="tech mb-5 mb-md-0 transition-all"
-        >
+        <div className="tech mb-5 mb-md-0 transition-all">
           <div className="icon-container m-auto">
             <img
               src="https://cdn-icons-png.flaticon.com/512/5968/5968322.png"
@@ -172,15 +131,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">Nodejs</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 1.1s cubic-bezier(0.17, 0.55, 0.55, 1) 1.1s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nestjs/nestjs-plain.svg"
@@ -190,15 +141,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">Nestjs</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 1.2s cubic-bezier(0.17, 0.55, 0.55, 1) 1.2s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
@@ -208,15 +151,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">MongoDB</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 1.3s cubic-bezier(0.17, 0.55, 0.55, 1) 1.3s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg"
@@ -226,15 +161,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">SASS</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 1.4s cubic-bezier(0.17, 0.55, 0.55, 1) 1.4s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://camo.githubusercontent.com/5734d0669fe22ce04a1cb989a156cd32c379875f6bca56d5210c9432824856d9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667"
@@ -244,15 +171,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">Tailwind</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
@@ -262,15 +181,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">Express</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 1.6s cubic-bezier(0.17, 0.55, 0.55, 1) 1.6s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg"
@@ -280,15 +191,7 @@ function Home() {
           </div>
           <p className="fs-2 text-center">Graphql</p>
         </div>
-        <div
-          ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 1.7s cubic-bezier(0.17, 0.55, 0.55, 1) 1.7s",
-          }}
-          className="tech mb-5 mb-md-0"
-        >
+        <div className="tech mb-5 mb-md-0">
           <div className="icon-container m-auto">
             <img
               src="https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667"
